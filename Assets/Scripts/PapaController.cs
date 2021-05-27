8fd97a2f7bc1cb118b9e95a549d1d7e91ad947ef
Xurya <xur1@bxscience.edu>
2021-05-26 23:48:37 -0400

    #4

---
+++ Assets/Scripts/PapaController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PapaController : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField]
+    private float speed = 0f;
+    private float basespeed = 10f;
+    public GameObject model;
+
+    [Header("Scream")]
+    [SerializeField]
+    private int stacks = 1;
+    [SerializeField] [Tooltip("Cooldown based on game clock.")]
+    private int hours = 2;
+    private static AudioClip screamAudio;
+    private AudioSource audioSrc; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = basespeed;
+        screamAudio = Resources.Load<AudioClip>("papaScream");
+        audioSrc = GetComponent<AudioSource>();
+        Scream();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Scream(){
+        audioSrc.PlayOneShot(screamAudio);
+        stacks++;
+        speed = basespeed*stacks;
+    }
+
+}
